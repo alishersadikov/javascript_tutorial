aaa1dbfed7003c06904793717311920660ea55a8
Alisher Sadikov <alishersadikov@gmailcom>
2016-10-31 22:20:32 -0600

    first commit

---
+++ lesson1_hw.js
@@ -0,0 +1,1 @@
+var name = prompt('What is your first name?'); var lastName = prompt('What is your last name?'); alert('Hi, '+name+' '+lastName+'!');
---
+++ lesson2_hw.js
@@ -0,0 +1,9 @@
+var age = prompt()
+
+if (age < 16) {
+  alert('you can\'t drive');
+} else if (age == 16) {
+  alert('you can drive FINALLY!');
+} else {
+  alert('you\'ve probably been driving already');
+}
